5d4a67111a4c23a8582250ebef0174dab0017122
xuxingyuan <1215325161@qq.con>
2017-10-25 20:03:43 +0800

    zy

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var fs=require('fs')
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*")
+var temp=''
+if(req.body.abc==1){
+	temp=[1,2,3]
+}else{
+	temp=[4,5,6]
+}
+res.send([temp])
+
+});
+
+module.exports = router;
+
